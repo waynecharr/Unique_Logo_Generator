7865c6f598ec15810f8bbd533c48555c56970043
waynecharr <137343274+waynecharr@users.noreply.github.com>
2023-09-06 16:56:51 -0400

    updated index.js with inquirer prompts, fixed issue with package.json

--- README.md
+++ README.md
@@ -4,6 +4,9 @@
 
 # Table of Contents
 
+"initial commit" initial commit with no code
+"updated index.js with inquirer prompts, fixed issue with package.json"
+
 # Update History 
 
 # Video 
--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-const inquirer = require("inquirer");
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateSVG =require("./lib/shapes.js")
+
+const questions = [
+    {
+        type: 'input',
+        name: 'initials',
+        message: 'Please enter three initials.',
+     },
+     {
+        type: 'input',
+        name: 'initial-color',
+        message: 'Please enter your preferred initial color',
+     },
+     {
+        type: 'input',
+        name: 'color',
+        message: 'Please enter your preferred shape color.',
+     },
+    {
+        type: 'list',
+        message: 'What is your preferred Shape?',
+        name: 'Shape',
+        choices: ['Circle', 'Square', 'Triangle'],
+      },
+]
+
+function writeToFile(data) {
+    fs.writeFile("./utils/logo.svg", data, (err) =>
+    err ? console.log(err) : console.log('Success!')
+)}
+
+function init() {
+    inquirer.prompt(questions)
+    // .then(function(data))
+    .then((data) => {
+        // console.log(data);
+        const template = generateSVG(data)
+        console.log(template);
+        writeToFile(template)
+
+    }) 
+}
+
+init();
