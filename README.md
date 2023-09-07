f5b2df96ff38d64d2f9c3399af3477f89733c87f
waynecharr <137343274+waynecharr@users.noreply.github.com>
2023-09-07 17:03:08 -0400

    updated the dimensions of some of the shapes, updated the test conditions to pass, and added example logos to the examples folder

--- README.md
+++ README.md
@@ -4,13 +4,22 @@
 
 # Table of Contents
 
-"initial commit" initial commit with no code
-"updated index.js with inquirer prompts, fixed issue with package.json"
-"added shape consts in shapes.js and started the function for generate svg, still not working properly." 
-"logo.svg now properly generating. Need to fix errors with the dimensions for the different sizes"
-"updated code to include classes, updated the svg generator with if statements to properly generate the logos" 
-
 # Update History 
+
+- "initial commit" initial commit with no code
+- "updated index.js with inquirer prompts, fixed issue with package.json"
+- "added shape consts in shapes.js and started the function for generate svg, still - not working properly." 
+- "logo.svg now properly generating. Need to fix errors with the dimensions for the - different sizes"
+- "updated code to include classes, updated the svg generator with if statements to properly generate the logos" 
+- "updated shape dimensions in shapes.js" 
+- "finished running tests for circle, square, and triangle. All run successfully."
+- "updated the dimensions of some of the shapes, updated the test conditions to pass, and added example logos to the examples folder"
+
+# Contributors
+
+- Worked with tutor Katie Hobbs. I had not been properly using classes, she corrected that as well as helped me get started with the testing functionality. 
+
+# Instructions
 
 # Video 
 
--- index.js
+++ index.js
@@ -10,21 +10,21 @@ const questions = [
     {
         type: 'input',
         name: 'initials',
-        message: 'Please enter three initials.',
+        message: 'Please enter three initials:',
      },
      {
         type: 'input',
         name: 'initialColor',
-        message: 'Please enter your preferred initial color',
+        message: 'Please enter your preferred initial color (or hexidecimal code):',
      },
      {
         type: 'input',
         name: 'shapeColor',
-        message: 'Please enter your preferred shape color.',
+        message: 'Please enter your preferred shape color (or hexidecimal code):',
      },
     {
         type: 'list',
-        message: 'What is your preferred Shape?',
+        message: 'Please select your preferred shape:',
         name: 'shape',
         choices: ['Circle', 'Square', 'Triangle'],
       },
@@ -59,71 +59,4 @@ function init() {
 }
 
 init();
-
-
-// function generateSVG(data) {
-  
-//     let svgShape; 
-//     svgShape = new Circle();
-//     console.log(svgShape);
-  
-//   }
-  
-//   function writeToFile(svgTemplate) {
-//     fs.writeFileSync('logo.svg', svgTemplate);
-//     console.log('Generated logo.svg');
-//   }
-  
-//   function init() {
-//     inquirer.prompt(questions).then((data) => {
-//         let svgShape; 
-//         svgShape = new Circle("WAC", "BLUE", "WHITE");
-//         //Make an if statement that revalutes this depending on what the user asks for
-//         const svg = svgShape.render();
-//         console.log(svg);
-
-
-
-   
-//       writeToFile(svg);
-//     });
-//   }
-  
-//   init();
-
-
-   // const shapeFunctions = {
-    //   Circle: shapesCircle,
-    //   Square: shapesSquare,
-    //   Triangle: shapesTriangle,
-    // };
-  
-    // const selectedShapeFunction = shapeFunctions[data.shape];
-  
-    // if (!selectedShapeFunction) {
-    //   console.error('Invalid shape selected.');
-    //   return '';
-    // }
-    // const shape = new Shapes();
-    // shape.createText(data.initials, data.initialColor);
-    // shape.createShape(svgShape);
-  
-    // return shape.render();
-
-// }
-// render(){
-//     return `
-//         <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">
-//         ${this.shape}
-//         ${this.text}
-//         </svg>
-//     `;
-// }
-// createText(text,color){
-//     this.text = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</>`
-// }
-// createShape(shape){
-//     this.shape = shape;
-// }
-// }
   
