8af6556cef37155219a4539a1671d4824cb60c7c
waynecharr <137343274+waynecharr@users.noreply.github.com>
2023-09-07 15:41:02 -0400

    updated code to include classes, updated the svg generator with if statements to properly generate the logos

--- README.md
+++ README.md
@@ -8,6 +8,7 @@
 "updated index.js with inquirer prompts, fixed issue with package.json"
 "added shape consts in shapes.js and started the function for generate svg, still not working properly." 
 "logo.svg now properly generating. Need to fix errors with the dimensions for the different sizes"
+"updated code to include classes, updated the svg generator with if statements to properly generate the logos" 
 
 # Update History 
 
--- index.js
+++ index.js
@@ -1,31 +1,10 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { Circle } = require("./lib/shapes.js")
+const { Circle, Square, Triangle } = require("./lib/shapes.js")
 
 // console.log("Circle:", Circle);
 // console.log("Square:", Square);
 // console.log("Triangle:", Triangle);
-
-class Shapes {
-    constructor(){
-        this.text = ''
-        this.shape = ''
-    }
-    render(){
-        return `
-            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">
-            ${this.shape}
-            ${this.text}
-            </svg>
-        `;
-    }
-    createText(text,color){
-        this.text = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</>`
-    }
-    createShape(shape){
-        this.shape = shape;
-    }
-}
 
 const questions = [
     {
@@ -52,7 +31,68 @@ const questions = [
 ]
 
 function generateSVG(data) {
-    // const shapeFunctions = {
+    let svgShape;
+    if (data.shape === 'Circle') {
+        svgShape = new Circle(data.initials, data.initialColor, data.shapeColor);
+    } else if (data.shape === 'Square') {
+        svgShape = new Square(data.initials, data.initialColor, data.shapeColor);
+    } else if (data.shape === 'Triangle') {
+        svgShape = new Triangle(data.initials, data.initialColor, data.shapeColor);
+    } else {
+        console.log('Invalid shape selection');
+        return;
+    }
+
+    const svg = svgShape.render();
+    writeToFile(svg);
+}
+
+function writeToFile(svgTemplate) {
+    fs.writeFileSync('logo.svg', svgTemplate);
+    console.log('Generated logo.svg');
+}
+
+function init() {
+    inquirer.prompt(questions).then((data) => {
+        generateSVG(data);
+    });
+}
+
+init();
+
+
+// function generateSVG(data) {
+  
+//     let svgShape; 
+//     svgShape = new Circle();
+//     console.log(svgShape);
+  
+//   }
+  
+//   function writeToFile(svgTemplate) {
+//     fs.writeFileSync('logo.svg', svgTemplate);
+//     console.log('Generated logo.svg');
+//   }
+  
+//   function init() {
+//     inquirer.prompt(questions).then((data) => {
+//         let svgShape; 
+//         svgShape = new Circle("WAC", "BLUE", "WHITE");
+//         //Make an if statement that revalutes this depending on what the user asks for
+//         const svg = svgShape.render();
+//         console.log(svg);
+
+
+
+   
+//       writeToFile(svg);
+//     });
+//   }
+  
+//   init();
+
+
+   // const shapeFunctions = {
     //   Circle: shapesCircle,
     //   Square: shapesSquare,
     //   Triangle: shapesTriangle,
@@ -64,36 +104,26 @@ function generateSVG(data) {
     //   console.error('Invalid shape selected.');
     //   return '';
     // }
-  
-    let svgShape; 
-    svgShape = new Circle();
-    console.log(svgShape);
-  
     // const shape = new Shapes();
     // shape.createText(data.initials, data.initialColor);
     // shape.createShape(svgShape);
   
     // return shape.render();
-  }
-  
-  function writeToFile(svgTemplate) {
-    fs.writeFileSync('logo.svg', svgTemplate);
-    console.log('Generated logo.svg');
-  }
-  
-  function init() {
-    inquirer.prompt(questions).then((data) => {
-        let svgShape; 
-        svgShape = new Circle("WAC", "BLUE", "WHITE");
-        //Make an if statement that revalutes this depending on what the user asks for
-        const svg = svgShape.render();
-        console.log(svg);
 
-
-
-    //   const template = generateSVG(data);
-      writeToFile(svg);
-    });
-  }
+// }
+// render(){
+//     return `
+//         <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+//         ${this.shape}
+//         ${this.text}
+//         </svg>
+//     `;
+// }
+// createText(text,color){
+//     this.text = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</>`
+// }
+// createShape(shape){
+//     this.shape = shape;
+// }
+// }
   
-  init();
